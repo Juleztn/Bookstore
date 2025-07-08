0501cd82feac5d7497b7ffc612914e3d7d321e79
Jule Zieten <ju-zie@web.de>
2025-07-08 20:10:13 +0200

    html template complete, style added

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function init() {
 
 function bookTemplate() {
     for (let i = 0; i < 3; i++) {
-        bookRef.innerHTML += renderBooks(i);     
+        bookRef.innerHTML += renderBooks(i);
     }
 }
 
@@ -15,20 +15,32 @@ function renderBooks(i) {
         <h2>${books[i].name}</h2>
         <img class="book-img" src="../assets/img/book.png" alt="">
         <div class="price-likes">
-            <p>${books[i].price}</p>
+            <p class="price">${books[i].price.toFixed(2)} €</p>
             <div class="likes">
-                <img class="heart-img" src="../assets/icons/heart-solid.svg" alt="">
-                <p>${books[i].likes}</p>
+                ${showIfLiked(i)}
+                <p class="price">${books[i].likes}</p>
             </div>
         </div>
         <div class="book-infos">
-            <div class="infos"><p>Autor:</p>${books[i].author}</div>
-            <div class="infos"><p>Erscheinungsjahr:</p>${books[i].publishedYear}</div>
-            <div class="infos"><p>Genre:</p>${books[i].genre}</div>
+            <div class="info-keys">
+                <p>Autor:</p>
+                <p>Erscheinungsjahr:</p>
+                <p>Genre:</p>
+            </div>
+            <div class="info-value">
+                <p>${books[i].author}</p>
+                <p>${books[i].publishedYear}</p>
+                <p>${books[i].genre}</p>
+            </div>
         </div>
-        <div>
+        <div class="comments">
             <h3>Kommentare:</h3>
-            <div>${showComments(i)}</div>
+            <div class="send-comment">
+                <input type="text"> 
+                <button>Senden</button>
+            </div>
+            <hr>
+            <div class="comment-section">${showComments(i)}</div>
         </div>
         </div>
     `;
@@ -36,16 +48,25 @@ function renderBooks(i) {
 
 function showComments(i) {
     if (books[i].comments.length === 0) {
-        return `<p>Noch keine Kommentare</p>`
+        return `<p><i>Noch keine Kommentare vorhanden</i></p>`
     } else {
+        let html = '';
         for (let indexComments = 0; indexComments < books[i].comments.length; indexComments++) {
-            return `<p>${books[i].comments[indexComments].name}</p>
-                <p>${books[i].comments[indexComments].comment}</p>`
-            }
-        
+            html += `<div class="comments-user-comment"><p><b>${books[i].comments[indexComments].name}:</b></p>
+                <p>${books[i].comments[indexComments].comment}</p></div>`
+        }
+        return html;
     }
 }
 
+function showIfLiked(i) {
+    if (books[i].liked == true) {
+        return `<img src="../assets/icons/heart-solid.svg" alt="">`
+    } else {
+        return `<img src="../assets/icons/heart-regular.svg" alt="">`
+    }
+}
 
-
+console.table(books[0]);
+console.table(books[1]);
 console.table(books[2]);
